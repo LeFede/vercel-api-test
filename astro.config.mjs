2715d4c3b8bebb4e9592471ab4b229ece265547f
LeFede <lefede@gmail.com>
2025-03-16 14:16:04 -0300

    go

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
+  adapter: netlify(),
   output: "server",
 });
