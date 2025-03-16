81662fb393012986378661624e0e3224232e886a
LeFede <lefede@gmail.com>
2025-03-16 14:06:23 -0300

    go

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,10 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 import type { APIRoute } from "astro";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 export const GET: APIRoute = async () => {
-  const tal = await prisma.day.findMany({});
+  const tal = await prisma.day.findMany({})
   return new Response(JSON.stringify(tal), {
     status: 200,
   });
